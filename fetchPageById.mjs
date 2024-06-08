1ace29f430098b5aa3370e6ae114c31a68d8598d
Adi Levi Malach <adi.levimalach@post.runi.ac.il>
2024-06-08 16:34:46 +0300

    Refactore fetchPageById

--- fetchPageById.mjs
+++ fetchPageById.mjs
@@ -1,51 +1,16 @@
-import https from 'https';
-import { URL } from 'url';
+import { makeApiRequest } from './utils/request.mjs';
 
-const email = 'adilevimalach@gmail.com';
-const apiToken =
-  'ATATT3xFfGF0TNsctHYmxmSTakAx5S7EOD9fB9-fSqULIzVbGxPcy25O97l5MTz4kSg0eW3d3gh33xmDhOHqHa-_UfrtUvk_CHnqC-Ui6yzhZ9e-ojx5YeOGjnlkO5-stvAvf-kSronkbczT7zwqMYnTE9oMPSlpRMoQCuXEzUxQ3pdxOYbAhlI=5AE0C5BD';
-const baseURL = 'https://adilevimalach.atlassian.net';
-const pageId = '884740'; // Replace with the actual page ID you want to fetch
-const path = `/wiki/api/v2/pages/${pageId}`;
+// TODO:handle this dynamically
+const pageId = '917509'; // Replace with the actual page ID
 
-const fetchPageById = () => {
-  const options = {
-    hostname: new URL(baseURL).hostname,
-    path,
-    method: 'GET',
-    headers: {
-      Authorization: `Basic ${Buffer.from(`${email}:${apiToken}`).toString(
-        'base64'
-      )}`,
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  };
-
-  const req = https.request(options, (res) => {
-    let data = '';
-
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    res.on('end', () => {
-      console.log(`Response: ${res.statusCode} ${res.statusMessage}`);
-      console.log('Response Headers:', res.headers);
-      try {
-        const jsonResponse = JSON.parse(data);
-        console.log('Response Body:', jsonResponse);
-      } catch (error) {
-        console.error('Error parsing JSON:', error.message);
-      }
-    });
-  });
-
-  req.on('error', (e) => {
-    console.error(`Problem with request: ${e.message}`);
-  });
-
-  req.end();
+const fetchPageById = async () => {
+  const path = `/wiki/rest/api/content/${pageId}`;
+  try {
+    const response = await makeApiRequest(path);
+    console.log('Fetched page:', response);
+  } catch (error) {
+    console.error('Error fetching page:', error.message);
+  }
 };
 
 // Run the function
